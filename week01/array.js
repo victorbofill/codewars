1b98286ed7783fe290484ce60d344e4a1aa83f3e
Victor Bofill <terranvas@gmail.com>
2018-02-27 08:49:08 -0800

    Moved the comments around and removed vestigial code

--- week01/array.js
+++ week01/array.js
@@ -2,7 +2,8 @@
 // You are going to be given an array of integers. Your job is to take that array and find an index N where the
 // sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no
 // index that would make this happen, return -1.
-const job = [1,2,3,4,3,2,1];
+
+// https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/javascript
 
 function findEvenIndex(arr) {
     let left = 0;
@@ -33,5 +34,3 @@ function findEvenIndex(arr) {
     }
 
 }
-findEvenIndex(job);
-// https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/javascript
